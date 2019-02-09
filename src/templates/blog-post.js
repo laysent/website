b7924b6d59f6512f6e565115358fdb69f0abbfe2
LaySent <laysent@gmail.com>
2019-02-09 16:46:24 +0800

    add: latex support for math equation in markdown

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,6 +5,8 @@ import Layout from '../components/Layout'
 import SEO from '../components/seo'
 import { rhythm, scale } from '../utils/typography'
 
+import 'katex/dist/katex.min.css';
+
 class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
