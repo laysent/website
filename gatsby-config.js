686b7a0daaefc0d12162fe9b1d7f929c074f8028
LaySent <laysent@gmail.com>
2019-02-11 13:18:39 +0800

    add: remark-sectionize plugin for markdown parser
    this will wrap up content under header in one <section>,
    which improves semantic explaination of HTML structure.

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,6 +56,7 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
+          `gatsby-remark-sectionize`,
         ],
       },
     },
