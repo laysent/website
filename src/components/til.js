d0c85d6e5a8cef3e854c4e12933845f7c125fa76
LaySent <laysent@gmail.com>
2019-04-11 22:18:24 +0800

    add: CC 4.0 License for TIL

--- src/components/til.js
+++ src/components/til.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { rhythm } from '../utils/typography'
+import cc from '../utils/cc.png';
 
 const TIL = ({ nodes }) => (
   <main>
@@ -23,6 +24,61 @@ const TIL = ({ nodes }) => (
         <hr />
       </article>
     ))}
+    <footer>
+      <div className="license" style={{ marginBottom: rhythm(1), fontSize: rhythm(0.5) }}>
+        <a
+          rel="license noopener noreferrer"
+          href="http://creativecommons.org/licenses/by-nc/4.0/"
+          target="_blank"
+        >
+          <img
+            alt="Creative Commons License"
+            src={cc}
+          />
+        </a>
+        <div>
+          {React.createElement('span', {
+            'xmlns:dct': 'http://purl.org/dc/terms/',
+            href: 'http://purl.org/dc/dcmitype/Text',
+            property: 'dct:title',
+            rel: 'dct:type',
+          }, 'Every blog post in this site')}
+          <span> by </span>
+          {React.createElement('a', {
+            'xmlns:cc': 'http://creativecommons.org/ns#',
+            href: 'https://github.com/laysent',
+            property: 'cc:attributionName',
+            rel: 'cc:attributionURL noopener noreferrer',
+            target: '_blank',
+          }, 'LaySent')}
+          <span> is licensed under a </span>
+          <a
+            rel="license noopener noreferrer"
+            href="http://creativecommons.org/licenses/by-nc/4.0/"
+            target="_blank"
+          >
+            CC 4.0 License
+          </a>
+          <span>, based on a work at </span>
+          {React.createElement('a', {
+            'xmlns:dct': 'http://purl.org/dc/terms/',
+            href: 'http://github.com/laysent/blog',
+            rel: 'dct:source noopener noreferrer',
+            target: '_blank',
+          }, 'GitHub')}
+          <span>. You can see the source code of this blog site </span>
+          <a
+            href="https://github.com/laysent/blog/tree/gh-pages"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            github.com/laysent/blog
+          </a>
+          <span>.</span>
+        </div>
+      </div>
+    </footer>
+    <hr />
   </main>
 );
 
